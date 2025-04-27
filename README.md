88e1c0243c1464e0a92ee369f6beaa9e93878bfc
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-27 23:30:35 +0900

    Update some files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # quasar-agent
-Quasar Agent
+
+ * quasar Agent using Agent Development Kit
+
+[Factoring](factoring.png)
+
+```shell
+adk web
+```
+
+## Installation
+
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+pip install google-adk
+gcloud auth application-default login
+```
+
+## Environment
+
+```
+# agent/.env
+GOOGLE_GENAI_USE_VERTEXAI=TRUE
+GOOGLE_CLOUD_PROJECT=YOUR_PROJECT_ID
+GOOGLE_CLOUD_LOCATION=us-central1
+COMMAND=/go/bin/quasar-mcp-server
+BASE_URL=https://CLOUD_RUN_URL.a.run.app
+GCLOUD_PATH=/google-cloud-sdk/bin/gcloud
+```
+
+## Links
+
+ * [Agent Development Kit](https://google.github.io/adk-docs/)
---
+++ agent/agent.py
@@ -0,0 +1,34 @@
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
+from google.adk.agents.llm_agent import LlmAgent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def get_tools():
+    tools, exit_stack = await MCPToolset.from_server(
+        connection_params=StdioServerParameters(
+            command=os.getenv("COMMAND"),
+            args=[],
+            env={
+                "BASE_URL": os.getenv("BASE_URL"),
+                "GCLOUD_PATH": os.getenv("GCLOUD_PATH"),
+            },
+        ),
+    )
+
+    return tools, exit_stack
+
+async def create_agent():
+    tools, exit_stack = await get_tools()
+    agent = LlmAgent(
+        name="quasar_agent",
+        model="gemini-2.0-flash",
+        description=("Agent to answer questions about the Quantum Computation and Quantum Information."),
+        instruction=("You are a helpful agent who can answer user questions about the Quantum Computation and Quantum Information."),
+        tools=tools,
+    )
+
+    return agent, exit_stack
+
+root_agent = create_agent()
