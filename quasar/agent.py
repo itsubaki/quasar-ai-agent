689f80fc74e9237bdc8cbd5a4e98b7d564664977
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-29 13:55:27 +0900

    Update quasar

--- quasar/agent.py
+++ quasar/agent.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-async def get_tools():
+async def get_quasar_tools():
     tools, exit_stack = await MCPToolset.from_server(
         connection_params=StdioServerParameters(
             command=os.getenv("COMMAND"),
@@ -20,13 +20,16 @@ async def get_tools():
     return tools, exit_stack
 
 async def create_agent():
-    tools, exit_stack = await get_tools()
+    quasar_tools, exit_stack = await get_quasar_tools()
     agent = Agent(
         name="quasar_agent",
         model="gemini-2.0-flash",
         description=("Agent to answer questions about the Quantum Computation and Quantum Information."),
         instruction=("You are a helpful agent who can answer user questions about the Quantum Computation and Quantum Information."),
-        tools=tools,
+        tools=[
+            *quasar_tools,
+            # other tools can be added here
+        ],
     )
 
     return agent, exit_stack
