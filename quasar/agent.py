1d45f13c156b3f1000f4984ce7f4d9704984c050
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-29 15:03:53 +0900

    Update agent

--- quasar/agent.py
+++ quasar/agent.py
@@ -1,8 +1,10 @@
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 from google.adk.agents.llm_agent import Agent
 from contextlib import AsyncExitStack
+from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()
 
@@ -21,6 +23,15 @@ async def get_quasar_tools(exit_stack):
 
     return tools
 
+def get_current_time(city: str, time_zone_id: str) -> dict:
+    tz = ZoneInfo(time_zone_id)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+
+    return {"status": "success", "report": report}
+
 async def create_agent():
     exit_stack = AsyncExitStack()
     quasar_tools = await get_quasar_tools(exit_stack)
@@ -30,7 +41,10 @@ async def create_agent():
         model="gemini-2.0-flash",
         description=("Agent to answer questions about the Quantum Computation and Quantum Information."),
         instruction=("You are a helpful agent who can answer user questions about the Quantum Computation and Quantum Information."),
-        tools=quasar_tools,
+        tools=[
+            *quasar_tools,
+            get_current_time,
+        ],
     )
 
     return agent, exit_stack
