f990bdfd0ab5b1c66e1411ddc3394353c6a9ff2b
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-03-01 22:29:52 +0900

    Update go mod

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"os"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"google.golang.org/adk/agent"
 	"google.golang.org/adk/agent/llmagent"
-	"google.golang.org/adk/cmd/launcher/adk"
+	"google.golang.org/adk/cmd/launcher"
 	"google.golang.org/adk/cmd/launcher/full"
 	"google.golang.org/adk/model/gemini"
-	"google.golang.org/adk/server/restapi/services"
 	"google.golang.org/adk/tool"
 	"google.golang.org/adk/tool/mcptoolset"
 	"google.golang.org/genai"
@@ -23,39 +23,34 @@ var (
 
 func main() {
 	ctx := context.Background()
-	model, err := gemini.NewModel(ctx, "gemini-2.5-flash", &genai.ClientConfig{
-		Project:  projectID,
-		Location: location,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	toolset, err := mcptoolset.New(mcptoolset.Config{
-		Transport: &mcp.StreamableClientTransport{
-			Endpoint: endpoint,
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	agent, err := llmagent.New(llmagent.Config{
-		Model:       model,
+	a := Must(llmagent.New(llmagent.Config{
 		Name:        "quantum_computation_specialist",
 		Description: "Answers user questions about the Quantum Computation and Quantum Information.",
 		Instruction: "You are a helpful agent who can answer user questions about the Quantum Computation and Quantum Information.",
+		Model: Must(gemini.NewModel(ctx, "gemini-2.5-flash", &genai.ClientConfig{
+			Project:  projectID,
+			Location: location,
+		})),
 		Toolsets: []tool.Toolset{
-			toolset,
+			Must(mcptoolset.New(mcptoolset.Config{
+				Transport: &mcp.StreamableClientTransport{
+					Endpoint: endpoint,
+				},
+			})),
 		},
-	})
+	}))
+
+	if err := full.NewLauncher().Execute(ctx, &launcher.Config{
+		AgentLoader: agent.NewSingleLoader(a),
+	}, os.Args[1:]); err != nil {
+		panic(err)
+	}
+}
+
+func Must[T any](v T, err error) T {
 	if err != nil {
 		panic(err)
 	}
 
-	if err = full.NewLauncher().Execute(ctx, &adk.Config{
-		AgentLoader: services.NewSingleAgentLoader(agent),
-	}, os.Args[1:]); err != nil {
-		panic(err)
-	}
+	return v
 }
